6ee02973546c304476477e14162f81f1538351f0
AMISHABANSAL06 <150264877+AMISHABANSAL06@users.noreply.github.com>
2024-07-20 13:35:46 +0530

    extract URL path

--- app/main.ts
+++ app/main.ts
@@ -4,10 +4,23 @@ import * as net from "net"; //used to create tcp servers and clients
 console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
+// const server = net.createServer((socket) => {
+//   //creates a new TCP server and takes a callbackfunction (event listener)
+//   socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`)); //uses write method to send data to the client which is a valid string and converts it into Buffer because the underlying TCP socket deals with binary data.
+//   socket.end();
+// });
+
 const server = net.createServer((socket) => {
-  //creates a new TCP server and takes a callbackfunction (event listener)
-  socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`)); //uses write method to send data to the client which is a valid string and converts it into Buffer because the underlying TCP socket deals with binary data.
-  socket.end();
+  socket.on("data", (data) => {
+    const dataS = data.toString();
+    const ans = dataS.split(" ")[1];
+    if (ans.charAt(0) === "/") {
+      socket.write(`HTTP/1.1 200 OK \r\n\r\n`);
+    } else {
+      socket.write(`HTTP/1.1 404 NOTFOUND \r\n\r\n`);
+    }
+    socket.end();
+  });
 });
 
 server.listen(4221, "localhost");
